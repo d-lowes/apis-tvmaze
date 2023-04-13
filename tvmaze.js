94886294d2aa06bba200b133d3cb8069bd5c89c0
John Chong <johnchong.99.17@gmail.com>
2023-04-12 19:15:42 -0500

    finish that bad boi

--- tvmaze.js
+++ tvmaze.js
@@ -4,7 +4,6 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 const BASE_URL = "http://api.tvmaze.com";
-
 const DOGE_IMG = "https://thedrum-media.imgix.net/thedrum-prod/s3/news/tmp/637022/shiba1.png?w=608&ar=default&fit=crop&crop=faces,edges&auto=format&dpr=1"
 
 /** Given a search term, search for tv shows that match that query.
@@ -13,15 +12,14 @@ const DOGE_IMG = "https://thedrum-media.imgix.net/thedrum-prod/s3/news/tmp/63702
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
-
 async function getShowsByTerm(searchTerm) {
-  let tvSeriesRequest = await axios.get(`${BASE_URL}/search/shows`, {
+  let tvShowsRequest = await axios.get(`${BASE_URL}/search/shows`, {
     params: {
       q: searchTerm
     }
   });
 
-  return tvSeriesRequest.data.map(showSearchResults => {
+  return tvShowsRequest.data.map(showSearchResults => {
     const showImg = showSearchResults.show.image
                     ? showSearchResults.show.image.medium
                     : DOGE_IMG;
@@ -33,13 +31,13 @@ async function getShowsByTerm(searchTerm) {
       image: showImg
     }
   });
+
 }
 
 /** Given list of shows, create markup for each and append to DOM.
  *
  * A show is {id, name, summary, image}
  * */
-
 function displayShows(shows) {
   $showsList.empty();
 
@@ -64,19 +62,20 @@ function displayShows(shows) {
 
     $showsList.append($show);
   }
+
 }
 
 
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
-
 async function searchShowsAndDisplay() {
   const term = $("#searchForm-term").val();
   const shows = await getShowsByTerm(term);
 
   $episodesArea.hide();
   displayShows(shows);
+
 }
 
 $searchForm.on("submit", async function handleSearchForm (evt) {
@@ -84,15 +83,14 @@ $searchForm.on("submit", async function handleSearchForm (evt) {
   await searchShowsAndDisplay();
 });
 
-
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
-
 async function getEpisodesOfShow(id) {
   const episodeData = await axios.get(
-    `${BASE_URL}/shows/${id}/episodes.`
+    `${BASE_URL}/shows/${id}/episodes`
   );
+
   return episodeData.data.map(episode => {
     return {
       id: episode.id,
@@ -101,10 +99,43 @@ async function getEpisodesOfShow(id) {
       number: episode.number
     }
   });
+
 }
 
-/** Accept an array of episodes ; append the episode info to the DOM */
+/** Accept an array of episodes; append the episode info and update
+ *  the DOM display
+ */
+function displayEpisodes(episodes) {
+  const episodeList = $("#episodesList");
 
-function displayEpisodes(episodes) { }
+  episodeList.empty();
+  $("#episodesArea").css("display", "block");
 
-// add other functions that will be useful / match our structure & design
+  for (let ep of episodes) {
+    let newEpisode = $("<li>");
+    newEpisode.text(`${ep.name} (season ${ep.season}, number ${ep.number})`);
+    episodeList.append(newEpisode);
+  }
+
+}
+
+/** Takes in a show id and utilizes both getEpisodesofShow() and displayEpisodes(),
+ *  acting as a conductor function. Activates when episode button is clicked
+ *  under any show
+ */
+async function getEpisodesAndDisplay(id) {
+  const episodesFromId = await getEpisodesOfShow(id);
+  displayEpisodes(episodesFromId);
+}
+
+/** Handles the click event on an 'episodes' button under a tv show series */
+function handleClickEpisodes(evt) {
+  //extract evt ancestor data.id point
+  let ancestorEpId = $(evt.target).closest(".Show").attr("data-show-id");
+
+  // console.log('ancestor id = ', ancestorEpId);
+  getEpisodesAndDisplay(ancestorEpId);
+
+}
+
+$("#showsList").on("click", "button", handleClickEpisodes);
